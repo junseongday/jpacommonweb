a0aa61dd83f90061d8957f4b06cf59db6b53c249
junseongday <junseongday@gmail.com>
2020-04-01 18:22:37 +0900

    Initial commit

---
+++ src/main/java/com/example/jpacommonweb/JpacommonwebApplication.java
@@ -0,0 +1,13 @@
+package com.example.jpacommonweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpacommonwebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JpacommonwebApplication.class, args);
+    }
+
+}
